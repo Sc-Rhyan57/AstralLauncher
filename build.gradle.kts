ba631acf538b6d40b574d04e9b10147f10822571
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-23 01:42:18 -0300

    feat: upload files via Android Dev Tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    kotlin("android") version "1.9.23" apply false
-    kotlin("plugin.serialization") version "1.9.23" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android)      apply false
+    alias(libs.plugins.kotlin.compose)      apply false
 }
