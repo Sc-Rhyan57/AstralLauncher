26b2061179c316615473d29bb35aa212164aa89a
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-23 01:41:37 -0300

    feat: upload files via Android Dev Tools

---
+++ app/src/main/java/com/astrallauncher/util/LuaRunner.kt
@@ -0,0 +1,7 @@
+package com.astrallauncher.util
+
+object LuaRunner {
+    fun execute(script: String): Result<String> {
+        return Result.success("Use o executor no overlay para rodar scripts via bridge TCP.")
+    }
+}
