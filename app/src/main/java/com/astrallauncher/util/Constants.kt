a23de0920619e5f8ffb22f4cdb709ed65d5ea1e6
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-25 19:57:44 -0300

    feat: upload files via Android Dev Tools

---
+++ app/src/main/java/com/astrallauncher/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.astrallauncher.util
+
+object Constants {
+    const val AU_PACKAGE = "com.innersloth.spacemafia"
+    const val PATCHED_PACKAGE = "com.astrallauncher.au"
+    const val PATCHED_LABEL = "Among Us ✦"
+    const val BRIDGE_PORT = 7777
+    const val BRIDGE_HOST = "127.0.0.1"
+}
