4e8e999eb1cd7a334acbc6d7fa3649e1031a6067
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-25 19:58:03 -0300

    Delete AstralBridge directory

--- AstralBridge/src/PluginInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace AstralBridge;
-
-internal static class PluginInfo
-{
-    internal const string GUID    = "com.astrallauncher.bridge";
-    internal const string NAME    = "AstralBridge";
-    internal const string VERSION = "1.0.0";
-}
