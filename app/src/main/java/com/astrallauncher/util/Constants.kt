544705462c724b86340d39767f4332d75405f5fe
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-25 20:27:18 -0300

    feat: upload files via Android Dev Tools

--- app/src/main/java/com/astrallauncher/util/Constants.kt
+++ app/src/main/java/com/astrallauncher/util/Constants.kt
@@ -2,8 +2,8 @@ package com.astrallauncher.util
 
 object Constants {
     const val AU_PACKAGE = "com.innersloth.spacemafia"
-    const val PATCHED_PACKAGE = "com.astrallauncher.au"
-    const val PATCHED_LABEL = "Among Us ✦"
-    const val BRIDGE_PORT = 7777
-    const val BRIDGE_HOST = "127.0.0.1"
+    const val PATCHED_SUFFIX = ".astral"
+    const val PATCHED_PACKAGE = "${AU_PACKAGE}${PATCHED_SUFFIX}"
+    const val MODS_DIR = "mods"
+    const val REPO_URL_DEFAULT = "https://raw.githubusercontent.com/astra1dev/awesome-among-us/main/mods.json"
 }
