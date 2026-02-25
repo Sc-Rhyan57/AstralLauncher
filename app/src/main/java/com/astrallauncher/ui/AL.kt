68387cca3595076faa1ecbf934e28f4af01e8605
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-25 20:50:33 -0300

    feat: upload files via Android Dev Tools

---
+++ app/src/main/java/com/astrallauncher/ui/AL.kt
@@ -0,0 +1,22 @@
+package com.astrallauncher.ui
+
+import androidx.compose.ui.graphics.Color
+
+object AL {
+    val Gold       = Color(0xFFFFD700)
+    val GoldDark   = Color(0xFFB8860B)
+    val GoldBg     = Color(0x22FFD700)
+    val Purple     = Color(0xFF9B59B6)
+    val PurpleBg   = Color(0x229B59B6)
+    val Bg         = Color(0xFF0D0D1A)
+    val Surface    = Color(0xFF1A1A2E)
+    val BgCard     = Color(0xFF1A1A2E)
+    val Border     = Color(0x22FFFFFF)
+    val White      = Color(0xFFFFFFFF)
+    val Muted      = Color(0x88FFFFFF)
+    val MutedL     = Color(0xAAFFFFFF)
+    val Success    = Color(0xFF00FF88)
+    val Error      = Color(0xFFFF6B6B)
+    val Warning    = Color(0xFFFFAA44)
+    val Info       = Color(0xFF4A90D9)
+}
